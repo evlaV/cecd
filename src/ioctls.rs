74cca5fdce4e0bbf6d8a95b642339aca8b906866
Vicki Pfau <vi@endrift.com>
2024-09-16 20:32:53 -0700

    Add ioctl definitions

--- src/ioctls.rs
+++ src/ioctls.rs
@@ -1,5 +1,8 @@
+use nix::{ioctl_read, ioctl_readwrite, ioctl_write_ptr};
 use std::ffi::c_char;
 
+use crate::log_addrs::CecLogicalAddresses;
+use crate::message::CecMessage;
 use crate::{Capabilities, EventFlags, LogicalAddressMask, Timestamp};
 
 /// CEC capabilities structure.
@@ -95,3 +98,47 @@ pub(crate) struct CecEvent {
     flags: EventFlags,
     data: CecEventUnion,
 }
+
+/* Adapter capabilities */
+ioctl_readwrite!(adapter_get_capabilities, b'a', 0, CecCapabilities);
+
+/*
+ * phys_addr is either 0 (if this is the CEC root device)
+ * or a valid physical address obtained from the sink's EDID
+ * as read by this CEC device (if this is a source device)
+ * or a physical address obtained and modified from a sink
+ * EDID and used for a sink CEC device.
+ * If nothing is connected, then phys_addr is 0xffff.
+ * See HDMI 1.4b, section 8.7 (Physical Address).
+ *
+ * The CEC_ADAP_S_PHYS_ADDR ioctl may not be available if that is handled
+ * internally.
+ */
+ioctl_read!(adapter_get_physical_address, b'a', 1, u16);
+ioctl_write_ptr!(adapter_set_physical_address, b'a', 2, u16);
+
+/*
+ * Configure the CEC adapter. It sets the device type and which
+ * logical types it will try to claim. It will return which
+ * logical addresses it could actually claim.
+ * An error is returned if the adapter is disabled or if there
+ * is no physical address assigned.
+ */
+ioctl_read!(adapter_get_logical_addresses, b'a', 3, CecLogicalAddresses);
+ioctl_readwrite!(adapter_set_logical_addresses, b'a', 4, CecLogicalAddresses);
+
+/* Transmit/receive a CEC command */
+ioctl_readwrite!(transmit_message, b'a', 5, CecMessage);
+ioctl_readwrite!(receive_message, b'a', 6, CecMessage);
+
+/* Dequeue CEC events */
+ioctl_readwrite!(dequeue_event, b'a', 7, CecEvent);
+
+/*
+ * Get and set the message handling mode for this filehandle.
+ */
+ioctl_read!(get_mode, b'a', 8, u32);
+ioctl_write_ptr!(set_mode, b'a', 9, u32);
+
+/* Get the connector info */
+ioctl_read!(adapter_get_connector_info, b'a', 10, CecConnectorInfo);
