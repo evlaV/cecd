353ff96e814278f99cd0da3c650d3ced4436fad2
Vicki Pfau <vi@endrift.com>
2024-11-13 21:11:46 -0800

    cecd: More bringup

--- cecd/src/dbus.rs
+++ cecd/src/dbus.rs
@@ -1,20 +1,130 @@
 use anyhow::Result;
-use zbus::interface;
-use linux_cec::device::AsyncDevice;
-use std::path::Path;
+use linux_cec::device::{AsyncDevice, PollResult, PollTimeout};
+use linux_cec::message::{self, Message};
+use linux_cec::operand::Version;
+use linux_cec::Timeout;
+use std::path::{Path, PathBuf};
+use std::sync::Arc;
+use tokio::fs::canonicalize;
+use tokio::select;
+use tokio::sync::Mutex;
+use tokio::task::{spawn, JoinHandle};
+use tokio_util::sync::CancellationToken;
+use zbus::object_server::{InterfaceRef, SignalEmitter};
+use zbus::{interface, Connection};
 
-struct CecDevice {
-    device: AsyncDevice,
+use crate::SystemHandle;
+
+const PATH: &'static str = "/com/steampowered/CecDaemon1";
+
+pub struct CecDevice {
+    device: Arc<Mutex<AsyncDevice>>,
+    token: CancellationToken,
+    poller: Option<JoinHandle<Result<()>>>,
+    path: PathBuf,
+}
+
+struct PollTask {
+    device: Arc<Mutex<AsyncDevice>>,
+    token: CancellationToken,
+    interface: InterfaceRef<CecDevice>,
+    system: SystemHandle,
 }
 
 impl CecDevice {
-    pub(crate) async fn open(path: impl AsRef<Path>) -> Result<CecDevice> {
+    pub async fn open(path: impl AsRef<Path>) -> Result<CecDevice> {
+        let path = canonicalize(path).await?;
+        let device = Arc::new(Mutex::new(AsyncDevice::open(&path).await?));
+        let token = CancellationToken::new();
         Ok(CecDevice {
-            device: AsyncDevice::open(&path).await?
+            device,
+            token,
+            path,
+            poller: None,
         })
+    }
+
+    pub async fn register(&mut self, connection: Connection, system: SystemHandle) -> Result<()> {
+        let object_server = connection.object_server();
+        let path = self.dbus_path()?;
+        let interface = object_server.interface(path).await?;
+        let poll_task = PollTask {
+            device: self.device.clone(),
+            token: self.token.clone(),
+            interface,
+            system,
+        };
+        self.poller = Some(spawn(poll_task.run()));
+        Ok(())
+    }
+
+    pub fn dbus_path(&self) -> Result<String> {
+        todo!();
     }
 }
 
-#[interface(name = "com.steampowered.CecDaemon.CecDevice")]
+#[interface(name = "com.steampowered.CecDaemon1.CecDevice1")]
 impl CecDevice {
+    #[zbus(signal)]
+    async fn received_message(
+        signal_emitter: &SignalEmitter<'_>,
+        initiator: u8,
+        destination: u8,
+        timestamp: u64,
+        message: &[u8],
+    ) -> zbus::Result<()>;
+}
+
+impl PollTask {
+    async fn run(self) -> Result<()> {
+        let poller = self.device.lock().await.get_poller().await?;
+        loop {
+            select! {
+                ev = poller.poll(PollTimeout::NONE) => {
+                    self.handle_poll_result(ev?).await?
+                }
+                _ = self.token.cancelled() => (),
+            }
+        }
+    }
+
+    async fn handle_poll_result(&self, result: PollResult) -> Result<()> {
+        if !result.got_message() {
+            return Ok(());
+        }
+        let envelope = self
+            .device
+            .lock()
+            .await
+            .rx_message(Timeout::from_ms(10))
+            .await?;
+
+        self.interface
+            .received_message(
+                envelope.initiator.into(),
+                envelope.destination.into(),
+                envelope.timestamp,
+                envelope.message.to_bytes().as_ref(),
+            )
+            .await?;
+
+        let reply =
+            match envelope.message {
+                Message::GetCecVersion(_) => Some(Message::CecVersion(message::CecVersion {
+                    version: Version::V2_0,
+                })),
+                Message::GiveDeviceVendorId(_) => self.system.vendor_id().await.map(|vendor_id| {
+                    Message::DeviceVendorId(message::DeviceVendorId { vendor_id })
+                }),
+                Message::GiveOsdName(_) => Some(Message::SetOsdName(message::SetOsdName {
+                    name: self.system.osd_name().await,
+                })),
+                Message::GivePhysicalAddr(_) => todo!(),
+                Message::UserControlPressed(_) => todo!(),
+                Message::UserControlReleased(_) => todo!(),
+                _ => None,
+            };
+
+        todo!();
+    }
 }
--- cecd/src/main.rs
+++ cecd/src/main.rs
@@ -1,10 +1,39 @@
 use anyhow::Result;
 use clap::Parser;
 use linux_cec::device::AsyncDevice;
+use linux_cec::operand::{BufferOperand, VendorId};
+use std::str::FromStr;
+use std::sync::Arc;
 use tokio::fs::read_dir;
+use tokio::sync::Mutex;
 use tracing::{debug, error, info};
 
 pub(crate) mod dbus;
+
+#[derive(Debug)]
+struct System {
+    osd_name: String,
+    vendor_id: Option<VendorId>,
+}
+
+#[derive(Clone, Debug)]
+#[repr(transparent)]
+pub(crate) struct SystemHandle(Arc<Mutex<System>>);
+
+impl SystemHandle {
+    pub(crate) async fn osd_name(&self) -> BufferOperand {
+        let osd_name = &self.0.lock().await.osd_name;
+        let truncated = osd_name
+            .char_indices()
+            .map_while(|(index, ch)| if index <= 14 { Some(ch) } else { None })
+            .collect::<String>();
+        BufferOperand::from_str(truncated.as_str()).unwrap()
+    }
+
+    pub(crate) async fn vendor_id(&self) -> Option<VendorId> {
+        self.0.lock().await.vendor_id
+    }
+}
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -21,7 +50,7 @@ struct Arguments {
     allow_hotplug: bool,
 }
 
-async fn find_dev() -> Result<Option<AsyncDevice>> {
+async fn find_devs() -> Result<()> {
     let mut dir = read_dir("/dev").await?;
     while let Some(entry) = dir.next_entry().await? {
         let name = entry.file_name();
@@ -32,28 +61,16 @@ async fn find_dev() -> Result<Option<AsyncDevice>> {
         let path = entry.path();
         let pathname = path.display();
         debug!("Scanning cec device {pathname}");
-        match AsyncDevice::open(&path).await {
-            Ok(device) => {
-                info!("Found cec device at {pathname}");
-                return Ok(Some(device));
-            }
-            Err(e) => {
-                error!("Failed to attach to cec device at {pathname}: {e}");
-            }
-        }
+        todo!();
     }
-    Ok(None)
+    Ok(())
 }
 
 #[tokio::main]
 pub async fn main() -> Result<()> {
     let args = Arguments::parse();
 
-    let device = if let (false, Some(path)) = (args.allow_hotplug, args.device) {
-        Some(AsyncDevice::open(path).await?)
-    } else {
-        find_dev().await?
-    };
+    todo!();
 
     Ok(())
 }
