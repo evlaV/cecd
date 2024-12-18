49b03ad0fd8df5d9b5b5713b361fdf2ca619b495
Vicki Pfau <vi@endrift.com>
2024-12-17 18:39:28 -0800

    cectool: Add some basic docs

--- linux-cec/src/bin/cectool.rs
+++ linux-cec/src/bin/cectool.rs
@@ -11,39 +11,67 @@ use linux_cec::{InitiatorMode, LogicalAddress, Result};
 use std::str::FromStr;
 
 #[derive(Parser)]
-#[command(version, about, long_about = None)]
+#[command(
+    version,
+    about,
+    long_about = "Basic tool for interfacing with the Linux CEC subsystem"
+)]
 struct Arguments {
     #[command(subcommand)]
     command: Command,
 
+    /// Which CEC device to use
     #[arg(short, long, default_value_t = String::from("/dev/cec0"))]
     device: String,
 }
 
 #[derive(Subcommand)]
 enum Command {
+    /// Get the connector information from the CEC subsystem
     GetConnectorInfo,
+    /// Get the physical address of the CEC adapter
     GetPhysicalAddress,
+    /// Get the logical address of the CEC adapter
     GetLogicalAddress,
-    SetLogicalAddress { log_addr: LogicalAddress },
+    /// Set the logical address of the CEC adapter
+    SetLogicalAddress {
+        /// The desired logical address
+        log_addr: LogicalAddress,
+    },
+    /// Clear the logical address of the CEC adapter
     ClearLogicalAddress,
-    SetOsdName { name: String },
+    /// Set the name displayed of this device as displayed on an OSD
+    SetOsdName {
+        /// The desired name, with a maximum of 14 bytes
+        name: String,
+    },
+    /// Make this the active device
     SetActive,
+    /// Standby all connected devices
     Standby,
+    /// Increase volume
     VolumeUp {
+        /// The logical address of the target device
         #[arg(default_value_t = LogicalAddress::Tv)]
         target: LogicalAddress,
     },
+    /// Decrease volume
     VolumeDown {
+        /// The logical address of the target device
         #[arg(default_value_t = LogicalAddress::Tv)]
         target: LogicalAddress,
     },
+    /// Toggle mute
     Mute {
+        /// The logical address of the target device
         #[arg(default_value_t = LogicalAddress::Tv)]
         target: LogicalAddress,
     },
-    SendKey {
+    /// Send a specified user command key
+    SendCommand {
+        /// The name of the specified command key
         key: UiCommand,
+        /// The logical address of the target device
         #[arg(default_value_t = LogicalAddress::Tv)]
         target: LogicalAddress,
     },
@@ -106,7 +134,7 @@ fn main() -> Result<()> {
             dev.press_user_control(UiCommand::Mute, target)?;
             dev.release_user_control(target)?;
         }
-        Command::SendKey { key, target } => {
+        Command::SendCommand { key, target } => {
             dev.set_initiator(InitiatorMode::Enabled)?;
             dev.press_user_control(key, target)?;
             dev.release_user_control(target)?;
