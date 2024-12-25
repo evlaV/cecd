b11bb78920a0d1bc0bf1376b4123f41f5aa70a3c
Vicki Pfau <vi@endrift.com>
2024-12-24 18:41:56 -0800

    linux-cec/cdc: Cleanup, add operand tests

--- linux-cec/src/cdc.rs
+++ linux-cec/src/cdc.rs
@@ -6,22 +6,39 @@
 #![allow(clippy::len_without_is_empty)]
 
 use bitfield_struct::bitfield;
+#[cfg(test)]
+use linux_cec_macros::opcode_test;
 use linux_cec_macros::{BitfieldSpecifier, MessageEnum, Operand};
 use num_enum::{IntoPrimitive, TryFromPrimitive};
 
 use crate::operand::OperandEncodable;
 use crate::{constants, operand, PhysicalAddress, Result};
+#[cfg(test)]
+use crate::{Error, Range};
 
 #[derive(BitfieldSpecifier, Debug, Copy, Clone, PartialEq, Eq, Hash)]
 #[bits = 2]
 #[repr(u8)]
-pub enum EncFunctionalityState {
+pub enum CdcErrorCode {
+    NoError = constants::CEC_OP_HPD_ERROR_NONE,
+    InitiatorNotCapable = constants::CEC_OP_HPD_ERROR_INITIATOR_NOT_CAPABLE,
+    InitiatorIncapableState = constants::CEC_OP_HPD_ERROR_INITIATOR_WRONG_STATE,
+    Other = constants::CEC_OP_HPD_ERROR_OTHER,
+}
+
+#[derive(BitfieldSpecifier, Debug, Copy, Clone, PartialEq, Eq, Hash)]
+#[bits = 2]
+#[repr(u8)]
+pub enum FunctionalityState {
     NotSupported = constants::CEC_OP_ENC_FUNC_STATE_EXT_CON_NOT_SUPPORTED,
     Inactive = constants::CEC_OP_ENC_FUNC_STATE_EXT_CON_INACTIVE,
     Active = constants::CEC_OP_ENC_FUNC_STATE_EXT_CON_ACTIVE,
     #[default]
     Invalid(u8),
 }
+
+pub type EncFunctionalityState = FunctionalityState;
+pub type HostFunctionalityState = FunctionalityState;
 
 #[derive(BitfieldSpecifier, Debug, Copy, Clone, PartialEq, Eq, Hash)]
 #[bits = 2]
@@ -33,29 +50,45 @@ pub enum HecFunctionalityState {
     ActivationField = constants::CEC_OP_HEC_FUNC_STATE_ACTIVATION_FIELD,
 }
 
-// TODO: Unit tests
 #[bitfield(u8)]
 #[derive(PartialEq, Eq, Hash, Operand)]
 pub struct HecState {
     #[bits(2)]
-    pub hec_functionality: HecFunctionalityState,
-    #[bits(2)]
-    pub host_functionality: HostFunctionalityState,
+    pub cdc_error: CdcErrorCode,
     #[bits(2)]
     pub enc_functionality: EncFunctionalityState,
     #[bits(2)]
-    _reserved: usize,
+    pub host_functionality: HostFunctionalityState,
+    #[bits(2)]
+    pub hec_functionality: HecFunctionalityState,
 }
 
-#[derive(BitfieldSpecifier, Debug, Copy, Clone, PartialEq, Eq, Hash)]
-#[bits = 2]
-#[repr(u8)]
-pub enum HostFunctionalityState {
-    NotSupported = constants::CEC_OP_HOST_FUNC_STATE_NOT_SUPPORTED,
-    Inactive = constants::CEC_OP_HOST_FUNC_STATE_INACTIVE,
-    Active = constants::CEC_OP_HOST_FUNC_STATE_ACTIVE,
-    #[default]
-    Invalid(u8),
+#[cfg(test)]
+mod test_hec_state {
+    use super::*;
+
+    opcode_test! {
+        ty: HecState,
+        instance: HecState::new()
+            .with_cdc_error(CdcErrorCode::NoError)
+            .with_enc_functionality(EncFunctionalityState::Inactive)
+            .with_host_functionality(HostFunctionalityState::Active)
+            .with_hec_functionality(HecFunctionalityState::ActivationField),
+        bytes: [0xE4],
+        extra: [Overfull],
+    }
+
+    #[test]
+    fn test_decode_empty() {
+        assert_eq!(
+            HecState::try_from_bytes(&[]),
+            Err(Error::OutOfRange {
+                got: 0,
+                expected: Range::AtLeast(1),
+                quantity: "bytes"
+            })
+        );
+    }
 }
 
 #[derive(BitfieldSpecifier, Debug, Copy, Clone, PartialEq, Eq, Hash)]
@@ -85,7 +118,6 @@ pub enum HpdState {
     Invalid(u8),
 }
 
-// TODO: Unit tests
 #[bitfield(u8)]
 #[derive(PartialEq, Eq, Hash, Operand)]
 pub struct InputPortHpdState {
@@ -95,7 +127,32 @@ pub struct InputPortHpdState {
     pub input_port: usize,
 }
 
-// TODO: Unit tests
+#[cfg(test)]
+mod test_input_port_hpd_state {
+    use super::*;
+
+    opcode_test! {
+        ty: InputPortHpdState,
+        instance: InputPortHpdState::new()
+            .with_input_port(0xA)
+            .with_state(HpdState::EdidDisableEnable),
+        bytes: [0xA5],
+        extra: [Overfull],
+    }
+
+    #[test]
+    fn test_decode_empty() {
+        assert_eq!(
+            InputPortHpdState::try_from_bytes(&[]),
+            Err(Error::OutOfRange {
+                got: 0,
+                expected: Range::AtLeast(1),
+                quantity: "bytes"
+            })
+        );
+    }
+}
+
 #[bitfield(u8)]
 #[derive(PartialEq, Eq, Hash, Operand)]
 pub struct HpdStateErrorCode {
@@ -105,7 +162,32 @@ pub struct HpdStateErrorCode {
     pub state: HpdState,
 }
 
-// TODO: Unit tests
+#[cfg(test)]
+mod test_hpd_state_error_code {
+    use super::*;
+
+    opcode_test! {
+        ty: HpdStateErrorCode,
+        instance: HpdStateErrorCode::new()
+            .with_state(HpdState::EdidDisableEnable)
+            .with_error_code(HpdErrorCode::InitiatorNotCapable),
+        bytes: [0x51],
+        extra: [Overfull],
+    }
+
+    #[test]
+    fn test_decode_empty() {
+        assert_eq!(
+            HpdStateErrorCode::try_from_bytes(&[]),
+            Err(Error::OutOfRange {
+                got: 0,
+                expected: Range::AtLeast(1),
+                quantity: "bytes"
+            })
+        );
+    }
+}
+
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
 pub struct HecField {
     pub input: [bool; 14],
@@ -136,36 +218,69 @@ impl OperandEncodable for HecField {
     }
 
     fn len(&self) -> usize {
-        1
+        2
     }
 }
 
-pub type HecSupportField = HecField;
-pub type HecActivationField = HecField;
+#[cfg(test)]
+mod test_hec_field {
+    use super::*;
+
+    opcode_test! {
+        ty: HecField,
+        instance: HecField {
+            input: [
+                 true, false, false, false,
+                false,  true, false, false,
+                false, false,  true,  true,
+                false, false,
+            ],
+            output: true,
+        },
+        bytes: [0x4C, 0x21],
+        extra: [Overfull],
+    }
+
+    #[test]
+    fn test_decode_empty() {
+        assert_eq!(
+            HecField::try_from_bytes(&[]),
+            Err(Error::OutOfRange {
+                got: 0,
+                expected: Range::AtLeast(2),
+                quantity: "bytes"
+            })
+        );
+    }
+}
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, MessageEnum)]
 #[repr(u8)]
 pub enum Message {
+    // TODO: Unit tests
     HecInquireState {
         terminating_address1: PhysicalAddress,
         terminating_address2: PhysicalAddress,
     } = constants::CEC_MSG_CDC_HEC_INQUIRE_STATE,
+    // TODO: Unit tests
     HecReportState {
         physical_address: PhysicalAddress,
         state: HecState,
-        support: HecSupportField,
-        activation: HecActivationField,
+        field: Option<HecField>,
     } = constants::CEC_MSG_CDC_HEC_REPORT_STATE,
+    // TODO: Unit tests
     HecSetStateAdjacent {
         terminating_address: PhysicalAddress,
         set_state: bool,
     } = constants::CEC_MSG_CDC_HEC_SET_STATE_ADJACENT,
+    // TODO: Unit tests
     HecSetState {
         terminating_address1: PhysicalAddress,
         terminating_address2: PhysicalAddress,
         set_state: bool,
         terminating_addresses: operand::BoundedBufferOperand<3, PhysicalAddress>,
     } = constants::CEC_MSG_CDC_HEC_SET_STATE,
+    // TODO: Unit tests
     HecRequestDeactivation {
         terminating_address1: PhysicalAddress,
         terminating_address2: PhysicalAddress,
@@ -173,7 +288,9 @@ pub enum Message {
     } = constants::CEC_MSG_CDC_HEC_REQUEST_DEACTIVATION,
     HecNotifyAlive = constants::CEC_MSG_CDC_HEC_NOTIFY_ALIVE,
     HecDiscover = constants::CEC_MSG_CDC_HEC_DISCOVER,
+    // TODO: Unit tests
     HpdSetState(InputPortHpdState) = constants::CEC_MSG_CDC_HPD_SET_STATE,
+    // TODO: Unit tests
     HpdReportState(HpdStateErrorCode) = constants::CEC_MSG_CDC_HPD_REPORT_STATE,
 }
 
